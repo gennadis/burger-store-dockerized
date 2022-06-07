16d914c5444ee0ad1e521e789fb8abe610e5fb05
Gennady Badinov <gennady.badinov@gmail.com>
2022-06-07 18:16:13 +0300

    Collect static files on containter building stage

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 set -e
 
-echo "Waiting for postgres..."
+echo "----- 1. Waiting for Postgres... -----"
 while ! nc -z $POSTGRES_HOST $POSTGRES_PORT; do
     sleep 0.1
 done
-echo "PostgreSQL started"
+echo "Postgres started."
+
+echo "----- 2. Collecting static files... -----"
+python manage.py collectstatic --noinput
+echo "Static files collected."
 
 exec "$@"
