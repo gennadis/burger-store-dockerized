fe17e5dba769cee67c6b8dd6c8093ca3167cfd52
Gennady Badinov <gennady.badinov@gmail.com>
2022-06-10 15:18:07 +0300

    Fixes...

--- production/init-letsencrypt.sh
+++ production/init-letsencrypt.sh
@@ -4,7 +4,7 @@ domains=(gennadis.ru www.gennadis.ru)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="gennady.badinov@gmail.com" # Adding a valid address is strongly recommended
-staging=0                # Set to 1 if you're testing your setup to avoid hitting request limits
+staging=1                # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
     read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
@@ -36,7 +36,7 @@ for domain in "${domains[@]}"; do
 done
 
 echo "### Starting nginx ..."
-docker-compose up --force-recreate -d
+docker-compose -f docker-compose.prod.yaml up --force-recreate -d
 echo
 
 for domain in "${domains[@]}"; do
