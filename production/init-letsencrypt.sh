955d30aaecb4fd0e8d1c144de54e0962aeb83f77
Gennady Badinov <gennady.badinov@gmail.com>
2022-06-10 10:57:50 +0300

    Fix `docker compose` installation checking

--- production/init-letsencrypt.sh
+++ production/init-letsencrypt.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e 
 
-if ! [ -x "$(command -v docker-compose)" ]; then
-  echo 'Error: docker-compose is not installed.' >&2
+if ! [ -x "$(command -v docker compose)" ]; then
+  echo 'Error: docker compose is not installed.' >&2
   exit 1
 fi
 
